675237db9906a0b78b858e05ad06ae74b95500d0
ahmedbabe <ahmedbou1900@gmail.com>
2021-10-03 19:45:27 +0000

    added serverless functions

---
+++ serverless/get_coords.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+
+const { WEATHER_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+    const params = JSON.parse(event.body);
+    const { text, units} = params;
+    const regex = /^\d+$/g;
+    const flag = regex.test(text) ? "zip" : "q";
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    const encodedUrl = encodeURI(url);
+    try {
+        const dataSteam = await fetch(encodedUrl);
+        const jsonData = await dataSteam.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(jsonData)
+        }
+    }
+}
